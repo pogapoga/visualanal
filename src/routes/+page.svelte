1825fe8fa42e00231b2046d7a098724b2f37a45c
Aniket <anik1224mau@gmail.com>
2024-11-07 13:16:58 +0530

    a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,6 @@
 <h1>hello </h1>
-import Papa from 'papaparse'
+<script>
+    const { data } = $props()
+</script>
 
-export async function load({ fetch, params }) {
-    const responseFlights = await fetch('https://jan-to.github.io/WebDataVis/flights_part.csv', 
-      { headers: { 'Content-Type': 'text/csv' } })
-    let csvFlights = await responseFlights.text()
-    let parsedCsvFlights = Papa.parse(csvFlights, {header: true})
-
-    return { flights: parsedCsvFlights.data }
+{JSON.stringify(data)}
