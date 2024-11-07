b7b3d8ffe97bbdc9bf731557b678af3f613a466e
Aniket <anik1224mau@gmail.com>
2024-11-07 12:45:52 +0530

    a

---
+++ src/routes/+page.js
@@ -0,0 +1,5 @@
+export function load({ params }) {
+    return {
+      values: [1,2,3,"a string"]
+    }
+  }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
-<h1>Visual analytics project 1</h1>
 <script>
-    let values = [1,2,3,"a string"]
-  </script>
-  
-  {JSON.stringify(values)}
+    const { data } = $props()
+</script>
+
+{JSON.stringify(data)}
