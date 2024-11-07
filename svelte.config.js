7b05061a1ba4590766baa2dd69167756ca2a36df
Aniket <anik1224mau@gmail.com>
2024-11-07 11:48:21 +0530

    hi

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/vizanal'
+			base: process.argv.includes('dev') ? '' : '/visualanal'
 		}
 	}
 };
