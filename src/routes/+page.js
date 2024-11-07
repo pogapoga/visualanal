cf11262afc74b7440100c404aab23c7eb587878b
Aniket <anik1224mau@gmail.com>
2024-11-07 13:05:18 +0530

    a

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,9 @@
-export function load({ params }) {
-    return {
-      values: [1,2,3,"a string"]
-    }
-  }
+import Papa from 'papaparse'
+
+export async function load({ fetch, params }) {
+    const responseFlights = await fetch('https://jan-to.github.io/WebDataVis/flights_part.csv', 
+      { headers: { 'Content-Type': 'text/csv' } })
+    let csvFlights = await responseFlights.text()
+    let parsedCsvFlights = Papa.parse(csvFlights, {header: true})
+
+    return { flights: parsedCsvFlights.data }
