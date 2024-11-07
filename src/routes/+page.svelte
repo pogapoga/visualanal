5f09f944f917aee80c78e457fba7550c23e15ad1
Aniket <anik1224mau@gmail.com>
2024-11-07 15:14:45 +0530

    a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,32 @@
 <script>
+  import { scaleLinear } from 'd3-scale';
+
   const { data } = $props();
-  
-  const rescale = function(x, domain_min, domain_max, range_min, range_max) {
-      return ((range_max - range_min)*(x-domain_min))/(domain_max-domain_min) + range_min
-  }
+
+  const scaleX = scaleLinear().domain([-180,180]).range([0,800])
+  const scaleY = scaleLinear().domain([-90,90]).range([400,0])
+  const scaleRadius = scaleLinear().domain([1,15406]).range([2,10])
   </script>
-  
-  <svg width="800" height="400">
+
+<svg width="800" height="400">
   {#each data.flights as datapoint}
-      <circle cx={rescale(datapoint.from_long, -180, 180, 0, 800)}
-              cy={rescale(datapoint.from_lat, -90, 90, 0, 400)}
-              r=3 />
+    <circle cx={scaleX(datapoint.from_long)}
+        cy={scaleY(datapoint.from_lat)}
+        r={scaleRadius(datapoint.distance)}
+        class:international={datapoint.from_country != datapoint.to_country}/>
   {/each}
-  </svg>
-  
-  <style>
-  circle {
-      fill: steelblue;
-      fill-opacity: 0.5;
+</svg>
+
+<style>
+  svg {
+    border: 1px;
+    border-style: solid;
   }
-  </style>
+  circle {
+    fill: steelblue;
+    fill-opacity: 0.5;
+  }
+  circle.international {
+    fill: red;
+  }
+</style>
