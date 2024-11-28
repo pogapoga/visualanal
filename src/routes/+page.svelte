affedc3f2f06fdae7aa56a4c7e55f2b5d560b862
Aniket <anik1224mau@gmail.com>
2024-11-28 17:16:06 +0530

    asd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -176,8 +176,8 @@
   <div id="victory-chart" class="chart"></div>
   
 
-  <button on:click={() => goto('/visualanal//pie')}>Victory type Pie Chart</button>
+  <button on:click={() => goto('/visualanal/pie')}>Victory type Pie Chart</button>
   
-  <button on:click={() => goto('/visualanal//openings')}>Top openings Bar Chart</button>
+  <button on:click={() => goto('/visualanal/openings')}>Top openings Bar Chart</button>
   <button on:click={() => goto('/visualanal/winner')}>Winners Donut Chart</button>
 </div>
