a3ffd69b5999671f2a7d30428d25084700e0b730
Aniket <anik1224mau@gmail.com>
2025-01-16 19:08:27 +0530

    asdsad

--- src/routes/time/+page.svelte
+++ src/routes/time/+page.svelte
@@ -2,80 +2,109 @@
   import * as d3 from "d3";
   import { onMount } from "svelte";
 
-  let incrementCounts = [];
+  let gameCategories = [];
 
   onMount(async () => {
     const response = await fetch("./games.csv");
     const rawData = await response.text();
     const data = d3.csvParse(rawData);
 
-    incrementCounts = getFilteredIncrementCounts(data, ["1", "3", "5", "10", "30"]);
-    drawIncrementBarChart(incrementCounts);
+    gameCategories = categorizeGames(data);
+    drawBarChart(gameCategories);
   });
 
-  function getFilteredIncrementCounts(data, baseTimes) {
-    const counts = {};
+  function categorizeGames(data) {
+    const categories = {
+      Bullet: 0,
+      Blitz: 0,
+      Rapid: 0,
+      Classical: 0,
+    };
 
-    
-    data.forEach(game => {
+    data.forEach((game) => {
       const increment = game.increment_code;
-      const baseMinutes = increment.split("+")[0]; 
+      const baseMinutes = parseInt(increment.split("+")[0]);
 
-      if (baseTimes.includes(baseMinutes)) {
-       
-        if (!counts[baseMinutes]) {
-          counts[baseMinutes] = 0;
-        }
-
-        counts[baseMinutes] += 1;
+      if (baseMinutes <= 1) {
+        categories.Bullet += 1;
+      } else if (baseMinutes === 5) {
+        categories.Blitz += 1;
+      } else if (baseMinutes === 10) {
+        categories.Rapid += 1;
+      } else if (baseMinutes >= 10) {
+        categories.Classical += 1;
       }
     });
 
-    return Object.entries(counts).map(([baseMinutes, count]) => ({
-      baseMinutes,
-      count
-    }));
+    return categories;
   }
 
-  function drawIncrementBarChart(incrementCounts) {
-    const margin = { top: 20, right: 30, bottom: 40, left: 40 };
+  function drawBarChart(categories) {
+    const margin = { top: 20, right: 30, bottom: 40, left: 100 };
     const width = 600 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+    const height = 300 - margin.top - margin.bottom;
 
-    const x = d3.scaleBand()
-      .domain(incrementCounts.map(d => d.baseMinutes)) 
-      .range([0, width])
-      .padding(0.1);
-
-    const y = d3.scaleLinear()
-      .domain([0, d3.max(incrementCounts, d => d.count)])
-      .nice()
-      .range([height, 0]);
-
-    const svg = d3.select("#increment-bar-chart")
+    const svg = d3
+      .select("#category-bar-chart")
       .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    svg.selectAll(".bar")
-      .data(incrementCounts)
-      .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", d => x(d.baseMinutes))
-      .attr("y", d => y(d.count))
-      .attr("width", x.bandwidth())
-      .attr("height", d => height - y(d.count))
-      .attr("fill", "#4682B4");
+    const x = d3.scaleLinear()
+      .domain([0, d3.max(Object.values(categories))])
+      .range([0, width]);
 
-   
+    const y = d3.scaleBand()
+      .domain(Object.keys(categories))
+      .range([0, height])
+      .padding(0.1);
+
+    const colorScale = d3.scaleOrdinal()
+      .domain(Object.keys(categories))
+      .range(["#FF6347", "#32CD32", "#FFD700", "#4682B4"]);
+
+    const tooltip = svg.append("g")
+      .attr("class", "tooltip")
+      .style("visibility", "hidden");
+
+    tooltip.append("rect")
+      .attr("width", 50)
+      .attr("height", 30)
+      .attr("fill", "white")
+      .attr("stroke", "black")
+      .attr("rx", 5)
+      .attr("ry", 5);
+
+    tooltip.append("text")
+      .attr("x", 5)
+      .attr("y", 15)
+      .style("font-size", "12px")
+      .style("fill", "black");
+
+    svg.selectAll(".bar")
+      .data(Object.entries(categories))
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", 0)
+      .attr("y", d => y(d[0]))
+      .attr("width", d => x(d[1]))
+      .attr("height", y.bandwidth())
+      .attr("fill", d => colorScale(d[0]))
+      .on("mouseover", function(event, d) {
+        tooltip.style("visibility", "visible")
+          .attr("transform", `translate(${x(d[1]) + 10}, ${y(d[0]) + y.bandwidth() / 2})`);
+        tooltip.select("text").text(d[1]);
+      })
+      .on("mouseout", function() {
+        tooltip.style("visibility", "hidden");
+      });
+
     svg.append("g")
       .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(x))
-      .selectAll("text")
-      .style("font-size", "12px")
-      .style("text-anchor", "middle");
+      .call(d3.axisBottom(x));
 
     svg.append("g")
       .call(d3.axisLeft(y));
@@ -93,9 +122,13 @@
   }
 
   .bar:hover {
-    fill: #355f73;
+    opacity: 0.8;
+  }
+
+  .tooltip {
+    pointer-events: none;
   }
 </style>
 
-<h1>Game Time Control (Increment Code) Bar Chart</h1>
-<div id="increment-bar-chart"></div>
+<h1>Game Time Control Distribution</h1>
+<div id="category-bar-chart"></div>
