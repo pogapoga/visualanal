f70dfef616bdf76a4ab2a9e99f8949fe987a2199
Aniket <anik1224mau@gmail.com>
2025-02-06 02:07:47 +0530

    saw

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,8 @@
   import { Chess } from 'svelte-chess';
   import Chart from "chart.js/auto";
   import { onMount } from "svelte";
-  import TimeChart from './time/+page.svelte';
+  
+  
   
   let winChartInstance = null;
   let ratedChartInstance = null;
@@ -18,6 +19,8 @@
   let isChartVisible = false;
   let topOpenings = [];
   let timeCategoryChartInstance = null;
+  let whiteRatingChartInstance = null;
+  let turnsCategoryChartInstance =  null;
 
 
   function parseCSV(data) {
@@ -100,6 +103,8 @@ function updateChartWithSelectedMoves() {
   updateRatedChart("ratedChart", movesData, selectedMoves);
   updateVictoryStatusChart("victoryStatusChart", movesData, selectedMoves);
   updateTimeCategoryChart("timeCategoryChart", movesData, selectedMoves);
+  updateWhiteRatingChart("whiteRatingChart", movesData, selectedMoves);
+  updateTurnsCategoryChart("turnsCategoryChart", movesData, selectedMoves);
 }
 
 
@@ -169,7 +174,9 @@ function updateChessBoard() {
     updateWinChart("winChart1", movesData, selectedMoves);
     updateRatedChart("ratedChart", movesData, selectedMoves);
     updateVictoryStatusChart("victoryStatusChart", movesData, selectedMoves);
-    updateTimeCategoryChart("timeCategoryChart", movesData, selectedMoves); // This will now update based on selected moves
+    updateTimeCategoryChart("timeCategoryChart", movesData, selectedMoves);
+    updateWhiteRatingChart("whiteRatingChart", movesData, selectedMoves);
+    updateTurnsCategoryChart("turnsCategoryChart", movesData, selectedMoves);
     updateChessBoard();
   }
 }
@@ -229,7 +236,7 @@ function updateRatedChart(canvasId, gamesData, selectedMoves = []) {
       },
     });
   } else {
-    // If the chart is already initialized, just update it with new data
+    
     ratedChartInstance.data.datasets[0].data = [ratedCounts.Rated, ratedCounts.Unrated];
     ratedChartInstance.update();
   }
@@ -311,6 +318,163 @@ function updateTimeCategoryChart(canvasId, gamesData, selectedMoves = []) {
 }
 
 
+function updateWhiteRatingChart(canvasId, gamesData, selectedMoves = []) {
+  const canvas = document.getElementById(canvasId);
+  const ctx = canvas.getContext("2d");
+
+  // Initialize counters for the rating categories
+  const ratingCategories = {
+    LessThan1000: 0,
+    From1000To1500: 0,
+    From1500To2000: 0,
+    MoreThan2000: 0,
+  };
+
+  // Loop through the games data and classify them based on white player rating
+  gamesData.forEach((game) => {
+    if (game.moves) {
+      const gameMoves = game.moves.split(" ");
+      
+      // Check if the game's moves start with the selected moves
+      if (selectedMoves.every((move, index) => move === gameMoves[index])) {
+        if (game.white_rating) {
+          const whiteRating = parseInt(game.white_rating);
+
+          // Classify based on the rating
+          if (whiteRating >= 1000 && whiteRating < 1500) {
+            ratingCategories.From1000To1500++;
+          } else if (whiteRating >= 1500 && whiteRating < 2000) {
+            ratingCategories.From1500To2000++;
+          } else if (whiteRating >= 2000) {
+            ratingCategories.MoreThan2000++;
+          }
+        }
+      }
+    }
+  });
+
+  console.log("White Rating Categories Calculated:", ratingCategories); // Debugging log
+
+  // If the chart is not initialized, create a new one
+  if (!whiteRatingChartInstance) {
+    whiteRatingChartInstance = new Chart(ctx, {
+      type: "pie",
+      data: {
+        labels: [
+          
+          "elo<1500",
+          "elo<2000",
+          "elo>2000"
+        ],
+        datasets: [
+          {
+            data: [
+              ratingCategories.LessThan1000,
+              ratingCategories.From1000To1500,
+              ratingCategories.From1500To2000,
+              ratingCategories.MoreThan2000,
+            ],
+            backgroundColor: ["#E1F8F7", "#4BC0C0", "#3D9B9B", "#2A7A7A"],
+
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: false,
+        plugins: {
+          legend: {
+            display: true,
+          },
+        },
+      },
+    });
+  } else {
+    // If the chart is already initialized, just update it with new data
+    whiteRatingChartInstance.data.datasets[0].data = [
+      ratingCategories.LessThan1000,
+      ratingCategories.From1000To1500,
+      ratingCategories.From1500To2000,
+      ratingCategories.MoreThan2000,
+    ];
+    whiteRatingChartInstance.update();
+  }
+}
+
+function updateTurnsCategoryChart(canvasId, gamesData, selectedMoves = []) {
+  const canvas = document.getElementById(canvasId);
+  const ctx = canvas.getContext("2d");
+
+  // Initialize counters for the turns categories
+  const turnsCategories = { LessThan15: 0, Between15And30: 0, Between30And45: 0, MoreThan45: 0 };
+
+  // Loop through the games data and classify them based on the number of turns
+  gamesData.forEach((game) => {
+    if (game.moves) {
+      const gameMoves = game.moves.split(" ");
+
+      // Check if the game's moves start with the selected moves
+      if (selectedMoves.every((move, index) => move === gameMoves[index])) {
+        // Use the 'turns' data directly
+        const numberOfTurns = game.turns;
+
+        // Classify based on the number of turns
+        if (numberOfTurns < 15) {
+          turnsCategories.LessThan15++;
+        } else if (numberOfTurns >= 15 && numberOfTurns <= 30) {
+          turnsCategories.Between15And30++;
+        } else if (numberOfTurns > 30 && numberOfTurns <= 45) {
+          turnsCategories.Between30And45++;
+        } else if (numberOfTurns > 45) {
+          turnsCategories.MoreThan45++;
+        }
+      }
+    }
+  });
+
+  console.log("Turns Categories Calculated:", turnsCategories); // Debugging log
+
+  // If the chart is not initialized, create a new one
+  if (!turnsCategoryChartInstance) {
+    turnsCategoryChartInstance = new Chart(ctx, {
+      type: "pie",
+      data: {
+        labels: ["<15 Turns", "15-30 Turns", "30-45 Turns", ">45 Turns"],
+        datasets: [
+          {
+            data: [
+              turnsCategories.LessThan15,
+              turnsCategories.Between15And30,
+              turnsCategories.Between30And45,
+              turnsCategories.MoreThan45,
+            ],
+            backgroundColor: ["#E1F8F7", "#4BC0C0", "#3D9B9B", "#2A7A7A"], // Adjust the colors as needed
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: false,
+        plugins: {
+          legend: {
+            display: true,
+          },
+        },
+      },
+    });
+  } else {
+    // If the chart is already initialized, just update it with new data
+    turnsCategoryChartInstance.data.datasets[0].data = [
+      turnsCategories.LessThan15,
+      turnsCategories.Between15And30,
+      turnsCategories.Between30And45,
+      turnsCategories.MoreThan45,
+    ];
+    turnsCategoryChartInstance.update();
+  }
+}
+
+
 
 
 function updateVictoryStatusChart(canvasId, gamesData, selectedMoves) {
@@ -379,12 +543,7 @@ function updateVictoryStatusChart(canvasId, gamesData, selectedMoves) {
   function resetGame() {
     if (chess) chess.reset();
     selectedMoves = [];
-   
-
-    // Recalculate initial responses for the starting position
     const initialResponses = calculateResponses(movesData, []);
-
-    // Reset bar chart to initial state (showing all possible white moves)
     if (chartInstance) {
         chartInstance.data.labels = Object.keys(initialResponses);
         chartInstance.data.datasets[0].data = Object.values(initialResponses);
@@ -393,17 +552,12 @@ function updateVictoryStatusChart(canvasId, gamesData, selectedMoves) {
     } else {
         updateChart("moveChart1", "Responses to starting position", initialResponses);
     }
-
-    // Reset win chart to initial state
     updateWinChart("winChart1", movesData, []);
-
-    // Reset victory status chart to initial state
     updateVictoryStatusChart("victoryStatusChart", movesData, []);
-
-    // Reset rated chart
     updateRatedChart("ratedChart", movesData);
-    updateTimeCategoryChart("timeCategoryChart",movesData);
-    
+    updateTimeCategoryChart("timeCategoryChart", movesData, []);
+    updateTurnsCategoryChart("turnsCategoryChart", movesData, []);
+    updateWhiteRatingChart("whiteRatingChart", movesData, []);
 }
 
 
@@ -487,6 +641,8 @@ onMount(async () => {
     updateRatedChart("ratedChart", movesData);
     updateVictoryStatusChart("victoryStatusChart", movesData, []);
     updateTimeCategoryChart("timeCategoryChart", movesData);
+    updateWhiteRatingChart("whiteRatingChart",movesData);
+    updateTurnsCategoryChart("turnsCategoryChart", movesData);
 
   } catch (error) {
     console.error("Error loading or processing CSV file:", error);
@@ -499,10 +655,19 @@ onMount(async () => {
 </script>
 <style>
 
+#turnsCategoryChart{
+  width: 220px; 
+  height: 220px; 
+} 
 #timeCategoryChart{
   width: 200px; 
   height: 200px; 
 } 
+#whiteRatingChart{
+  width: 190px; 
+  height: 190px; 
+} 
+
 
 #victoryStatusChart {
   width: 220px; 
@@ -579,7 +744,7 @@ onMount(async () => {
 
   .chess-container {
     flex: 1;
-    max-width: 500px;
+    max-width: 590px;
     padding: 20px;
     background-color: #f9fafb;
     border-radius: 12px;
@@ -653,11 +818,18 @@ onMount(async () => {
 #timeCategoryChartInstanceContainer {
   position: absolute;
   right: 250px;
-  top: 530px;
+  top: 535px;
 }
-
-
-
+#turnsCategoryChartInstanceContainer {
+  position: absolute;
+  right: 250px;
+  top: 99px;
+}
+#whiteRatingChartContainer{
+  position: absolute;
+  right: 250px;
+  top: 320px;
+}
   #chart-container {
     display: block;
   }
@@ -691,12 +863,24 @@ onMount(async () => {
       <canvas id="timeCategoryChart"></canvas>
     </div>
   </div>
+  <div class="pie-chart-container" id="turnsCategoryChartInstanceContainer">
+    <div class="visualization-box">
+      <canvas id="turnsCategoryChart"></canvas>
+    </div>
+  </div>
   
+  
+  <div class="pie-chart-container" id="whiteRatingChartContainer">
+    <div class="visualization-box">
+      <canvas id="whiteRatingChart"></canvas>
+    </div>
+  </div>
   <div class="main-container">
     <div class="chess-container">
-      <Chess bind:this={chess}  />
+      <Chess bind:this={chess}   />
       <button on:click={resetGame}>Reset</button>
       <button on:click={() => chess?.undo()}>Undo</button>
+      <button on:click={() => chess?.toggleOrientation()}>Flip Board</button>
     </div>
 
     <div>
