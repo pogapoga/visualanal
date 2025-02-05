f757aba2d205c9861ca023332579da8d430dab4c
Aniket <anik1224mau@gmail.com>
2025-02-06 03:25:04 +0530

    dddas

--- src/routes/engine/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  import Chess, { Engine } from 'svelte-chess';
-  // Note: stockfish.js must be manually downloaded (see Readme)
-</script>
-<Chess engine={new Engine({depth: 20, moveTime: 1500, color: 'w'})} />
